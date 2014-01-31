08e87e1aadb4c6e27daf9b25d4a425c929e538aa
Steve Gotham <g23design@gmail.com>
2014-01-30 21:23:06 -0700

    Install devise, add a user, and install devise views

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,6 @@ Gbook::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+         config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
